d8e7ca2e2d83ed4b3bad364eacef85b1cfe960c1
ChoYunMin <dbsals0618@naver.com>
2017-12-05 10:35:11 +0900

    ActionBar complete

--- Secretary-master/Secretary-master/app/src/main/java/com/example/nawoo/secretaryproject/MainActivity.java
+++ Secretary-master/Secretary-master/app/src/main/java/com/example/nawoo/secretaryproject/MainActivity.java
@@ -8,8 +8,11 @@ package com.example.nawoo.secretaryproject;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,11 +24,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // ActionBar에 타이틀 변경
+        getSupportActionBar().setTitle("조윤민");
+        // ActionBar의 배경색 변경
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF891D32));
+
+        // shedule/friends 프래그먼트 변경
         android.app.FragmentManager fm = getFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
         fragmentTransaction.add(R.id.main_fragment_view, new Fragment_Schedule());
         fragmentTransaction.commit();
 
+        // 프래그먼트 변경 버튼
         Button buttonFragSwitch = (Button)findViewById(R.id.switch_button);
         buttonFragSwitch.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v){
@@ -33,6 +43,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.action_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        // ActionBar에 + 버튼을 클릭했을 때의 동작
+        return true;
     }
 
     public void switchFragment(){
