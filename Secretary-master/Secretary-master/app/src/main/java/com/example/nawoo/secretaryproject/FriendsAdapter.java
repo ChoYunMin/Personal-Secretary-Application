17ef0d21ab4fa807e1facc5d757e7744e8c31000
nawook96 <nawook0331@nate.com>
2017-12-17 21:05:42 +0900

    accept

--- Secretary-master/Secretary-master/app/src/main/java/com/example/nawoo/secretaryproject/FriendsAdapter.java
+++ Secretary-master/Secretary-master/app/src/main/java/com/example/nawoo/secretaryproject/FriendsAdapter.java
@@ -1,14 +1,23 @@
 package com.example.nawoo.secretaryproject;
 
 import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 /**
@@ -19,6 +28,7 @@ public class FriendsAdapter extends BaseAdapter {
     private LayoutInflater inflater;
     private ArrayList<FriendsItem> data;
     private Context mContext = null;
+    private int curPosition;
 
 
     public FriendsAdapter(Context context, ArrayList<FriendsItem> data){
@@ -34,6 +44,7 @@ public class FriendsAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
         View v = convertView;
+        curPosition = position;
         if(v == null){
             this.inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             v = inflater.inflate(R.layout.friends_list,parent,false);
@@ -76,15 +87,63 @@ public class FriendsAdapter extends BaseAdapter {
     private View.OnClickListener buttonClickListener = new View.OnClickListener(){
         @Override
         public void onClick(View v){
+
             switch(v.getId()){
                 case R.id.btn_accept:
-                    Toast.makeText(mContext,"버튼 Tag = " + v.getTag(), Toast.LENGTH_SHORT).show();
-
-
+                    modifyFriendDB mfd = new modifyFriendDB();
+                    mfd.execute();
                     break;
             }
         }
     };
+
+    public class modifyFriendDB extends AsyncTask<Void, Integer, Void> {
+        String data = "";
+        @Override
+        protected Void doInBackground(Void... unused) {
+
+/* 인풋 파라메터값 생성 */
+            String param = "u_id1=" + getItem(curPosition).getRequester()  + "&u_id2=" + getItem(curPosition).getID() +"";
+            try {
+/* 서버연결 */
+                URL url = new URL(
+                        "http://211.214.113.144:8888/Dproject/accept_friend.php");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.connect();
+
+/* 안드로이드 -> 서버 파라메터값 전달 */
+                OutputStream outs = conn.getOutputStream();
+                outs.write(param.getBytes("UTF-8"));
+                outs.flush();
+                outs.close();
+
+/* 서버 -> 안드로이드 파라메터값 전달 */
+                InputStream is = null;
+                BufferedReader in = null;
+
+                is = conn.getInputStream();
+                in = new BufferedReader(new InputStreamReader(is), 8 * 1024);
+                String line = null;
+                StringBuffer buff = new StringBuffer();
+                while ((line = in.readLine()) != null) {
+                    buff.append(line + "\n");
+                }
+                data = buff.toString().trim();
+                Log.e("RECV DATA", data);
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+    }
 
     @Override
     public long getItemId(int position){
